043e374420750835915604ed3bf0ae34cb6caad8
Aleksandar_Klipa <saki.94@hotmail.com>
2023-10-27 09:38:06 +0200

    Adding pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
