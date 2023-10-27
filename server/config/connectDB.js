6695aedd15961d772a8ca0b9f029021d6912d756
Aleksandar_Klipa <saki.94@hotmail.com>
2023-10-27 14:27:22 +0200

    Connect To Database

---
+++ server/config/connectDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.DATABASE_URL);
+
+        console.log('Connected to MongoDB')
+    } catch(error) {
+        console.log(error.message);
+
+        process.exit();
+    }   
+}
